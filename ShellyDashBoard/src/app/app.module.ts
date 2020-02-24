1b793899443b9fbeb43d509ae9ec3b3c17acfc4a
RickiestRick <patrickmeierroding@freenet.de>
2020-02-24 19:17:27 +0100

    Router with page not found

--- ShellyDashBoard/src/app/app.module.ts
+++ ShellyDashBoard/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PageNotFoundComponent } from './../PageNotFound/PageNotFound.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -10,7 +11,25 @@ import {MatIconModule} from '@angular/material/icon';
 import { ShellyListComponent } from './../shelly-list/shelly-list.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { RouterModule, Routes } from '@angular/router';
 import { ShellyListitemComponent } from './../shelly-listitem/shelly-listitem.component';
+
+const appRoutes: Routes = [
+
+   {
+     path: 'Shellies',
+     component: ShellyListComponent
+   },
+   { path: '',
+     redirectTo: '/Shellies',
+     pathMatch: 'full'
+   }
+   ,
+  { path: '**', component: PageNotFoundComponent }
+  
+ ];
+
+
 @NgModule({
    declarations: [
       AppComponent,
@@ -18,6 +37,10 @@ import { ShellyListitemComponent } from './../shelly-listitem/shelly-listitem.co
       ShellyListitemComponent
    ],
    imports: [
+      RouterModule.forRoot(
+         appRoutes,
+         { enableTracing: true } // <-- debugging purposes only
+       ),
       BrowserModule,
       MatCardModule,
       MatGridListModule,
